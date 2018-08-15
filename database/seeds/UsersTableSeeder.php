a0dfe8e59510860169c7122825f41872f6fce624
Lawrence Belo <lawrence.belo@fullspeedtechnologies.com>
2018-08-15 16:54:00 +0900

    Added initial seed data for users db table

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,21 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            [
+                'username'   => 'testuser1',
+                'first_name' => 'Test',
+                'last_name'  => 'User',
+                'role'       => 0,
+                'password'   => bcrypt('abcd1234')
+            ],
+            [
+                'username'   => 'testadmin1',
+                'first_name' => 'Test',
+                'last_name'  => 'Admin',
+                'role'       => 1,
+                'password'   => bcrypt('abcd1234')
+            ]
+        ]);
     }
 }
